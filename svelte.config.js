c295b0dce0b455f40ac752e37aada9cb91ea7b3e
Jerry Luo <jerryluoaustin@gmail.com>
2025-01-18 19:14:11 -0600

    change config

--- svelte.config.js
+++ svelte.config.js
@@ -6,9 +6,6 @@ const config = {
 		adapter: adapter({
 			fallback: '404.html'
 		}),
-		prerender: {
-			default: true
-		},
 		paths: {
 			base: process.argv.includes('dev') ? '' : process.env.BASE_PATH
 		}
